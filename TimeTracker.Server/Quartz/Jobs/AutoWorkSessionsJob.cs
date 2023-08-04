183224b2aaf66d5f0f711b83ca68239eee7c497c
Davyd Melnychenko <ipz204_mdv@student.ztu.edu.ua>
2023-08-04 14:44:11 +0300

    added quartz.net and operation with autoadding work session for fulltime worker

---
+++ TimeTracker.Server/Quartz/Jobs/AutoWorkSessionsJob.cs
@@ -0,0 +1,38 @@
+﻿using Quartz;
+using TimeTracker.Server.Data.Abstractions;
+using TimeTracker.Server.Data.Models.WorkSession;
+
+namespace TimeTracker.Server.Quartz.Jobs;
+
+public class AutoWorkSessionsJob : IJob
+{
+    private readonly IWorkSessionRepository _workSessionRepository;
+    private readonly IUserRepository _userRepository;
+
+    public AutoWorkSessionsJob(IWorkSessionRepository workSessionRepository, IUserRepository userRepository)
+    {
+        _workSessionRepository = workSessionRepository;
+        _userRepository = userRepository;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        var users = await _userRepository.GetFullTimeUsersAsync();
+        Console.WriteLine("AutoWorkSessionsJob working");
+
+        DateTime workSessionStart = DateTime.Today + new TimeSpan(5, 0, 0);
+        DateTime workSessionEnd = DateTime.Today + new TimeSpan(13, 0, 0);
+
+        foreach (var user in users)
+        {
+            var plannedWorkSession = new WorkSessionDataRequest()
+            {
+                UserId = user.Id,
+                Start = workSessionStart,
+                End = workSessionEnd,
+                Type = "Auto"
+            };
+            await _workSessionRepository.CreateWorkSessionAsync(plannedWorkSession);
+        }
+    }
+}
