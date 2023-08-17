09cb9973dd358785dd46dab672ddbb459e6a87d0
Davyd Melnychenko <ipz204_mdv@student.ztu.edu.ua>
2023-08-17 14:02:13 +0300

    finished data layer

---
+++ TimeTracker.Server.Data/Abstractions/ISickLeaveRepository.cs
@@ -0,0 +1,16 @@
+﻿using TimeTracker.Server.Data.Models.SickLeave;
+
+namespace TimeTracker.Server.Data.Abstractions;
+
+public interface ISickLeaveRepository
+{
+    public Task<SickLeaveDataResponse> GetSickLeaveByIdAsync(Guid id);
+
+    public Task<List<SickLeaveDataResponse>> GetSickLeavesAsync(DateTime date, Guid? userId, bool searchByYear = false);
+
+    public Task<SickLeaveDataResponse> CreateSickLeaveAsync(SickLeaveDataRequest sickLeaveDataRequest);
+
+    public Task UpdateSickLeaveAsync(Guid id, SickLeaveDataRequest sickLeaveDataRequest);
+
+    public Task DeleteSickLeaveAsync(Guid id);
+}
---
+++ TimeTracker.Server.Data/Repositories/SickLeaveRepository.cs
@@ -0,0 +1,114 @@
+﻿using Dapper;
+using TimeTracker.Server.Data.Abstractions;
+using TimeTracker.Server.Data.Models.SickLeave;
+
+namespace TimeTracker.Server.Data.Repositories;
+
+public class SickLeaveRepository : ISickLeaveRepository
+{
+    private readonly DapperContext _context;
+
+    public SickLeaveRepository(DapperContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<SickLeaveDataResponse> GetSickLeaveByIdAsync(Guid id)
+    {
+        const string query = $"SELECT * FROM [SickLeave] WHERE [{nameof(SickLeaveDataResponse.Id)}] = @{nameof(id)};";
+
+        using var connection = _context.GetConnection();
+        var sickLeaveDataResponse = await connection.QuerySingleOrDefaultAsync<SickLeaveDataResponse>(query, new { id });
+
+        return sickLeaveDataResponse;
+    }
+
+    public async Task<List<SickLeaveDataResponse>> GetSickLeavesAsync(DateTime date, Guid? userId, bool searchByYear = false)
+    {
+        var query = $"SELECT * FROM [SickLeave] WHERE ";
+
+        if (searchByYear)
+        {
+            query += $"YEAR([{nameof(SickLeaveDataResponse.Start)}]) = {date.Year}";
+        }
+        else
+        {
+            query += $"MONTH([{nameof(SickLeaveDataResponse.Start)}]) = {date.Month}";
+        }
+
+        if (userId is not null)
+        {
+            query += $" AND [{nameof(SickLeaveDataResponse.UserId)}] = '{userId}'";
+        }
+        query += ";";
+
+        using var connection = _context.GetConnection();
+        var sickLeavesDataResponse = await connection.QueryAsync<SickLeaveDataResponse>(query);
+
+        return sickLeavesDataResponse.ToList();
+    }
+
+    public async Task<SickLeaveDataResponse> CreateSickLeaveAsync(SickLeaveDataRequest sickLeaveDataRequest)
+    {
+        var id = Guid.NewGuid();
+
+        const string query = $"INSERT INTO [SickLeave] (" +
+            $"[{nameof(SickLeaveDataResponse.Id)}], " +
+            $"[{nameof(SickLeaveDataResponse.UserId)}], " +
+            $"[{nameof(SickLeaveDataResponse.LastModifierId)}], " +
+            $"[{nameof(SickLeaveDataResponse.Start)}], " +
+            $"[{nameof(SickLeaveDataResponse.End)}]" +
+            $") VALUES (" +
+            $"@{nameof(SickLeaveDataResponse.Id)}, " +
+            $"@{nameof(SickLeaveDataResponse.UserId)}, " +
+            $"@{nameof(SickLeaveDataResponse.LastModifierId)}, " +
+            $"@{nameof(SickLeaveDataResponse.Start)}, " +
+            $"@{nameof(SickLeaveDataResponse.End)});";
+
+        using var connection = _context.GetConnection();
+
+        await connection.ExecuteAsync(query, new
+        {
+            Id = id,
+            sickLeaveDataRequest.UserId,
+            sickLeaveDataRequest.LastModifierId,
+            sickLeaveDataRequest.Start,
+            sickLeaveDataRequest.End
+        });
+
+        var sickLeaveDataResponse = await GetSickLeaveByIdAsync(id);
+        if (sickLeaveDataResponse is null)
+        {
+            throw new InvalidOperationException("Sick leave data has not been added");
+        }
+
+        return sickLeaveDataResponse;
+    }
+
+    public async Task UpdateSickLeaveAsync(Guid id, SickLeaveDataRequest sickLeaveDataRequest)
+    {
+        const string query = $"UPDATE [SickLeave] SET " +
+            $"[{nameof(SickLeaveDataResponse.LastModifierId)}] = @{nameof(SickLeaveDataResponse.LastModifierId)}, " +
+            $"[{nameof(SickLeaveDataResponse.Start)}] = @{nameof(SickLeaveDataResponse.Start)}, " +
+            $"[{nameof(SickLeaveDataResponse.End)}] = @{nameof(SickLeaveDataResponse.End)} " +
+            $"WHERE [{nameof(SickLeaveDataResponse.Id)}] = @{nameof(SickLeaveDataResponse.Id)};";
+
+        using var connection = _context.GetConnection();
+
+        await connection.ExecuteAsync(query, new
+        {
+            Id = id,
+            sickLeaveDataRequest.LastModifierId,
+            sickLeaveDataRequest.Start,
+            sickLeaveDataRequest.End
+        });
+    }
+
+    public async Task DeleteSickLeaveAsync(Guid id)
+    {
+        const string query = $"DELETE FROM [SickLeave] WHERE [{nameof(SickLeaveDataResponse.Id)}] = @{nameof(id)};";
+
+        using var connection = _context.GetConnection();
+        await connection.ExecuteAsync(query, new { id });
+    }
+}
